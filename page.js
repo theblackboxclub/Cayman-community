d23bb38ea87111dcbd52353166aa31bd6a346ce0
theblackboxclub <emmanueltomkins@gmail.com>
2025-12-29 00:11:33 -0500

    Create page.js

---
+++ page.js
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to CaymanCircle</h1>
+      <p>
+        A local space for people in the Cayman Islands to connect for fitness,
+        business, and social life.
+      </p>
+      <p>
+        🚧 App under construction
+      </p>
+    </div>
+  );
+}
