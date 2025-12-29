34f057b97832350ee6c727688e761991f2d24209
theblackboxclub <emmanueltomkins@gmail.com>
2025-12-29 01:47:41 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  // This tells Next.js to correctly process the modern code in Firebase
+  transpilePackages: ['undici', 'firebase', '@firebase/auth'],
 }
 
 module.exports = nextConfig
