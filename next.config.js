6a6b3987345d9b047739f455a58b9f64b181afa5
theblackboxclub <emmanueltomkins@gmail.com>
2025-12-31 19:15:32 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // This line fixes the build error by translating the new code
   transpilePackages: ['undici', 'firebase', '@firebase/auth'],
 }
 
