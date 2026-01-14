0346f69b2ced3dc031c6be32af49615e9edfe7e7
theblackboxclub <emmanueltomkins@gmail.com>
2026-01-14 17:37:56 -0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,7 +1,7 @@
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage"; // <--- NEW IMPORT
+import { getStorage } from "firebase/storage"; 
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -16,6 +16,6 @@ const firebaseConfig = {
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const auth = getAuth(app);
 const db = getFirestore(app);
-const storage = getStorage(app); // <--- NEW SERVICE
+const storage = getStorage(app); 
 
-export { auth, db, storage }; 
+export { auth, db, storage };
