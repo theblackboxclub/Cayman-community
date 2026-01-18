61187a3f44e723e2470f255ad56a055211ecd524
theblackboxclub <emmanueltomkins@gmail.com>
2026-01-18 11:29:19 -0500

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,19 @@
 import './globals.css';
+import { Inter } from 'next/font/google';
+
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
   title: 'CircleCayman',
-  description: 'The community app for Grand Cayman.',
+  description: 'Community App for Cayman Islands',
+  manifest: '/manifest.json', // Linked here
+  viewport: 'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0', // Prevents zooming on inputs
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
